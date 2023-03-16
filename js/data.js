7dbaa829ca172ef7e2f21af57df5d25bc356992c
lwinkokointern1 <dkmadsintern1@gmail.com>
2023-03-16 16:52:21 +0630

    testing_shake

--- js/data.js
+++ js/data.js
@@ -1,10 +1,3 @@
-var image_array = [
-    "https://img.freepik.com/free-vector/cute-bad-cat-wearing-suit-sunglasses-with-baseball-bat-cartoon-icon-illustration-animal-fashion-icon-concept-isolated-flat-cartoon-style_138676-2170.jpg?w=2000",
-    "https://img.freepik.com/free-vector/cute-cool-baby-holding-teddy-bear-doll-cartoon-vector-icon-illustration-people-holiday-isolated_138676-5356.jpg?w=2000",
-    "https://img.freepik.com/free-vector/cute-astronaut-dance-cartoon-vector-icon-illustration-technology-science-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-3851.jpg?w=2000",
-    "https://i.guim.co.uk/img/media/c8b1d78883dfbe7cd3f112495941ebc0b25d2265/256_0_3840_2304/master/3840.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=3ac0dfad4c2edc23c843d59f1ec08cc8"
-];
-var i = 0;
 window.onload = function() {
     //create a new instance of shake.js.
     var myShakeEvent = new Shake({
@@ -23,10 +16,16 @@ window.onload = function() {
 
     $('span').bind( "touchstart", function(e){add();} );
 };
-
+var image_array = [
+    "https://img.freepik.com/free-vector/cute-bad-cat-wearing-suit-sunglasses-with-baseball-bat-cartoon-icon-illustration-animal-fashion-icon-concept-isolated-flat-cartoon-style_138676-2170.jpg?w=2000",
+    "https://img.freepik.com/free-vector/cute-cool-baby-holding-teddy-bear-doll-cartoon-vector-icon-illustration-people-holiday-isolated_138676-5356.jpg?w=2000",
+    "https://img.freepik.com/free-vector/cute-astronaut-dance-cartoon-vector-icon-illustration-technology-science-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-3851.jpg?w=2000",
+    "https://i.guim.co.uk/img/media/c8b1d78883dfbe7cd3f112495941ebc0b25d2265/256_0_3840_2304/master/3840.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=3ac0dfad4c2edc23c843d59f1ec08cc8"
+];
+var i = 0;
 add = function(){ 
-    while( i < image_array.length){
+    if(i < image_array.length){
         document.getElementById('image').src = image_array[i];
     }
-    i++
+    i++;
 }
