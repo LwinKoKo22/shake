28bdd3147e9dc97edc9629ef71120044289ebabe
lwinkokointern1 <dkmadsintern1@gmail.com>
2023-03-17 10:29:33 +0630

    update_data_js

--- js/data.js
+++ js/data.js
@@ -1,21 +1,9 @@
 window.onload = function() {
     //create a new instance of shake.js.
-    var myShakeEvent = new Shake({
-        threshold: 15
-    });
-    // start listening to device motion
+    var myShakeEvent = new Shake({threshold: 15});
     myShakeEvent.start();
-    // register a shake event
-    window.addEventListener('shake', shakeEventDidOccur, false);
-    //shake event callback
-    function shakeEventDidOccur () {
-        //put your own code here etc.
-      add();
-        //alert('Shake!');
-    }
-
-    $('span').bind( "touchstart", function(e){add();} );
-};
-add = function(){ 
-  document.getElementById('para').innerText ="I'm testing";
+    
+    window.addEventListener('shake', function(){
+      document.getElementById('para').innerHTML = "I'm testing";
+    }, false)
 }
