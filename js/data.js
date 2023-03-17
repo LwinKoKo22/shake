79e4ea85ea85ad5c706bd8d6f2833a7b23bfd1ea
lwinkokointern1 <dkmadsintern1@gmail.com>
2023-03-17 11:33:23 +0630

    testing

--- js/data.js
+++ js/data.js
@@ -1,9 +1,28 @@
 window.onload = function() {
-    //create a new instance of shake.js.
-    var myShakeEvent = new Shake({threshold: 15});
-    myShakeEvent.start();
-    
-    window.addEventListener('shake', function(){
+  // Check if DeviceMotionEvent is supported
+if (window.DeviceMotionEvent) {
+  // Add a listener for the device motion event
+  window.addEventListener('devicemotion', shakeEventReceived, false);
+}
+
+// Define the shakeEventReceived function
+function shakeEventReceived(event) {
+  // Check if the Shake.js library is loaded
+  if (typeof Shake !== 'undefined') {
+    // Create a new Shake instance
+    const shake = new Shake({threshold: 15});
+
+    // Start listening for shake events
+    shake.start();
+
+    // Add a listener for the shake event
+    window.addEventListener('shake', shakeEventDidOccur, false);
+
+    // Define the shakeEventDidOccur function
+    function shakeEventDidOccur() {
+      // Do something in response to the shake event
       document.getElementById('para').innerHTML = "I'm testing";
-    }, false)
+    }
+  }
+}
 }
