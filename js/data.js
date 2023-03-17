6d7d54418a41b919ff4776cbebf37454f0f75495
lwinkokointern1 <dkmadsintern1@gmail.com>
2023-03-17 10:17:22 +0630

    testing-update

--- js/data.js
+++ js/data.js
@@ -16,16 +16,6 @@ window.onload = function() {
 
     $('span').bind( "touchstart", function(e){add();} );
 };
-var image_array = [
-    "https://img.freepik.com/free-vector/cute-bad-cat-wearing-suit-sunglasses-with-baseball-bat-cartoon-icon-illustration-animal-fashion-icon-concept-isolated-flat-cartoon-style_138676-2170.jpg?w=2000",
-    "https://img.freepik.com/free-vector/cute-cool-baby-holding-teddy-bear-doll-cartoon-vector-icon-illustration-people-holiday-isolated_138676-5356.jpg?w=2000",
-    "https://img.freepik.com/free-vector/cute-astronaut-dance-cartoon-vector-icon-illustration-technology-science-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-3851.jpg?w=2000",
-    "https://img.freepik.com/free-vector/cute-swag-polar-bear-with-hat-gold-chain-necklace-cartoon-illustration-flat-cartoon-style_138676-2719.jpg?w=2000"
-];
-var i = 0;
 add = function(){ 
-    if(i < image_array.length){
-        document.getElementById('image').src = image_array[i];
-    }
-    i++;
+  document.getElementById('para').innerText ="I'm testing";
 }
